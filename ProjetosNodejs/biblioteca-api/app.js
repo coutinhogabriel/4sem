4a7b53df5a772322739ce42d36059bc4c1bf3b48
coutinhogabriel <gabriel.coutin@hotmail.com>
2024-08-28 21:53:38 -0300

    28/08/2024

--- ProjetosNodejs/biblioteca-api/app.js
+++ ProjetosNodejs/biblioteca-api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const livroRoutes = require('./routes/livroRoutes');
+const cors=require('cors');
 require('dotenv').config();
 require('./config/database'); // Conectando ao banco de dados
 
@@ -11,6 +12,7 @@ const app = express();
 // Middlewares
 app.use(bodyParser.json());
 
+app.use(cors());
 
 // Rotas
 app.use('/livros', livroRoutes);
